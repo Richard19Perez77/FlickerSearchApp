b13b87453f431e63f753f1d456fe40a1b51087a5
richa <richard19.perez77@yahoo.com>
2025-01-16 17:56:11 +0700

    comments added

--- app/src/main/java/com/rperez/flickersearchapp/navigation/Screen.kt
+++ app/src/main/java/com/rperez/flickersearchapp/navigation/Screen.kt
@@ -1,6 +1,30 @@
 package com.rperez.flickersearchapp.navigation
 
+/**
+ * Defines the navigation structure for the app.
+ * Each screen in the app is represented as a sealed class.
+ *
+ * @property route The route string used for navigation.
+ */
 sealed class Screen(val route: String) {
+
+    /**
+     * Represents the home screen of the app.
+     *
+     * Route: "home"
+     */
     object Home : Screen("home")
+
+    /**
+     * Represents the detail screen of the app.
+     *
+     * Route: "detail/{title}/{description}/{author}/{published}/{url}"
+     * The route contains placeholders for the following parameters:
+     * - **title**: The title of the item to display.
+     * - **description**: A description of the item.
+     * - **author**: The author of the item.
+     * - **published**: The published date of the item.
+     * - **url**: The URL of the item's associated content.
+     */
     object Detail : Screen("detail/{title}/{description}/{author}/{published}/{url}")
 }
